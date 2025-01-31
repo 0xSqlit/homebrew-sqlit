a4e1286f3f87d79898752408a059e1a36519a14c
Umar Ashfaq <xradgul@gmail.com>
2025-01-31 15:29:26 -0800

    Add version

--- Formula/sqlit.rb
+++ Formula/sqlit.rb
@@ -3,6 +3,7 @@ class Sqlit < Formula
   homepage "https://www.sqlit.dev"
   url "https://drive.google.com/file/d/16vmKq5EX2_g2kZwPc_xL8nf0oKWWgagA/view?usp=sharing"
   sha256 "faf9b40e70dfb05985e309861473daf87d076304828fc11bb99d3b168b72a9b8"
+  version "0.0.0"
   license "Proprietary"
 
   def install
