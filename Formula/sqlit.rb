78076ee1f678a606d68b6f7a29558ec9e62d2e35
Umar Ashfaq <xradgul@gmail.com>
2025-01-31 15:36:16 -0800

    Update download link

--- Formula/sqlit.rb
+++ Formula/sqlit.rb
@@ -1,7 +1,7 @@
 class Sqlit < Formula
   desc "A simple tool for querying JSON with SQL"
   homepage "https://www.sqlit.dev"
-  url "https://drive.google.com/file/d/16vmKq5EX2_g2kZwPc_xL8nf0oKWWgagA/view?usp=sharing"
+  url "https://drive.usercontent.google.com/u/0/uc?id=16vmKq5EX2_g2kZwPc_xL8nf0oKWWgagA&export=download"
   sha256 "faf9b40e70dfb05985e309861473daf87d076304828fc11bb99d3b168b72a9b8"
   version "0.0.0"
   license "Proprietary"
