38f7ba9d530173bac11af201ed8d9c91fc5df688
Umar Ashfaq <xradgul@gmail.com>
2025-01-31 19:43:25 -0800

    Bump version

--- Formula/sqlit.rb
+++ Formula/sqlit.rb
@@ -1,9 +1,9 @@
 class Sqlit < Formula
-  desc "A simple tool for querying JSON with SQL"
+  desc "A lightweight data processor that enables running SQL against non-database sources"
   homepage "https://www.sqlit.dev"
-  url "https://drive.usercontent.google.com/u/0/uc?id=16vmKq5EX2_g2kZwPc_xL8nf0oKWWgagA&export=download"
-  sha256 "faf9b40e70dfb05985e309861473daf87d076304828fc11bb99d3b168b72a9b8"
-  version "0.0.0"
+  url "https://drive.usercontent.google.com/u/0/uc?id=1Dj8Rrtbo8wAAkzXyWESSDmKc4s6elAQ1&export=download"
+  sha256 "dae099523b27e598ed7dfd961cede637ddb999277d61c702b52845e5b3caaca5"
+  version "0.0.1"
   license "Proprietary"
 
   def install
